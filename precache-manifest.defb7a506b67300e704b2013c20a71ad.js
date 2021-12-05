befdbdd76cc74a2c200564bf0e8109bfad5a6ddf
Moshiko-Start <moshiko14691@gmail.com>
2021-12-05 13:53:47 +0200

    Updates

---
+++ precache-manifest.defb7a506b67300e704b2013c20a71ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef642b1460b6090dc2095bb10a52199",
+    "url": "/stock-photos/index.html"
+  },
+  {
+    "revision": "beef6a51e702cb6c871c",
+    "url": "/stock-photos/static/css/main.a1c094ad.chunk.css"
+  },
+  {
+    "revision": "833aaddd2aad21d82868",
+    "url": "/stock-photos/static/js/2.f9fac789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stock-photos/static/js/2.f9fac789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beef6a51e702cb6c871c",
+    "url": "/stock-photos/static/js/main.5a4aa1c8.chunk.js"
+  },
+  {
+    "revision": "f78428a150c276ad98a9",
+    "url": "/stock-photos/static/js/runtime-main.a5d9fa22.js"
+  }
+]);
